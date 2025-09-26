ce2c4b9b295fadc3f22903ade68f326dcf8f483e
Alahi Majnur Osama <osama@bgctub.ac.bd>
2025-09-26 20:37:14 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "studio-6735051727-ce0b9",
+  "appId": "1:907990168113:web:26c266b21874efa4635b10",
+  "apiKey": "AIzaSyBu4iXxDcpF2u7h9bBxjkOCs2gWwY-ihQY",
+  "authDomain": "studio-6735051727-ce0b9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "907990168113"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
